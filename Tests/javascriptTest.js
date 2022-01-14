29981229b1c1367d755b56dd0758689ab28dece4
Nate Kliegl <nkliegl2@gmail.com>
2022-01-14 08:42:19 -0600

    JavaScript test case

---
+++ Tests/javascriptTest.js
@@ -0,0 +1,17 @@
+// Importing Builder, By, Key and util from the from package
+const { Builder, By, Key, util } = require("selenium-webdriver");
+
+require("chromedriver");
+
+// Writing the function for running the test
+async function test() {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  // Navigates to site
+  await driver.get("http://hudl.com/login");
+  await driver.findElement(By.name("email")).sendKeys(Key.RETURN);
+
+  await driver.findElement(by.name("password")).sendKeys(Key.RETURN);
+}
+// calling the function
+test();
